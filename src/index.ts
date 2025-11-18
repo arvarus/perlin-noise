fe4b5e807c45bfd45536082e49e838e3bbf6c322
PPRB <pprb@arvarus.com>
2025-11-18 13:25:43 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @arvarus/perlin-noise
+ * Perlin noise implementation in TypeScript
+ */
+
+export class PerlinNoise {
+  constructor(seed?: number) {
+    // TODO: Implement Perlin noise generator
+  }
+
+  /**
+   * Generate noise value at given coordinates
+   */
+  noise(x: number, y?: number, z?: number): number {
+    // TODO: Implement noise generation
+    return 0;
+  }
+}
+
